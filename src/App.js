7d086e6927c69aaab6fb31e5da3ef634ce912100
Adam <amkhadov2014@mail.ru>
2022-04-13 00:32:11 +0300

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 function App() {
   return (
     <div className="wrapper ">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="cartItem">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div>
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+          <div className="cartItem">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div>
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+        
+        
+        
+        
+         </div>
+         <ul>
+              <li className="ss">
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+        </div>
+      </div>
       <header>
         <div className="headerLeft">
           <img width={40} height={40} src="/img/logo.png" />
           <div className="infoNameShop">
             <h3>React sneakers</h3>
-            <p >Магазин лучших кроссовок</p>
+            <p>Магазин лучших кроссовок</p>
           </div>
         </div>
         <ul className="headerRigth headerBuutoms">
@@ -26,20 +69,24 @@ function App() {
         </ul>
       </header>
       <div className="content">
-      <div>
-      <h1>Все кроссовки</h1>
-      <div className="search-block">
-        <img width={16} height={16} src="/img/search-icon.png" alt="Search" />
-        <input placeholder="Поиск..." />
-  
+        <div>
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img
+              width={16}
+              height={16}
+              src="/img/search-icon.png"
+              alt="Search"
+            />
+            <input placeholder="Поиск..." />
+          </div>
         </div>
-      </div>
         <div className="products">
           <div className="card">
             <img
               width={200}
               height={180}
-              src="/img/sneakers/5.jpg"
+              src="/img/sneakers/1.jpg"
               alt="sneakers"
             />
             <h5>Кроссовки ECCO BIOM 2.0 M</h5>
